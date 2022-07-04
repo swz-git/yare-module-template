3db8a29e081e6dfb0101d695a78a468830d07496
swz-git <contact@swz.works>
2022-07-04 14:19:55 +0200

    change import order, client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,11 +1,11 @@
+import { client } from "yare-module-lib";
+
 // Inject some custom html
 // @ts-ignore editor doesn't understand custom esbuild loaders
 import html from "./index.html";
 client.injectHtml(html);
 
 // Communication through channels
-import { client } from "yare-module-lib";
-
 client.channels.addEventListener("Example module", (msgs) => {
   client.channels.send("Example module", "Hello from the client!");
   console.log(client.channels.read("Example module"));
